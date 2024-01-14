6c3b327102826af8c954faf5da2cbf8b7e7d5aa6
taufiqelahi <taufiqelahiiceiu@gmail.com>
2024-01-14 22:32:12 +0600

    added questions summary

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -5,35 +5,34 @@ import 'package:quiz_app_basics/data/questions.dart';
 class ResultScreen extends StatelessWidget {
   final List<String> onChoose;
   const ResultScreen({super.key, required this.onChoose});
+  List<Map<String,dynamic>>getSummaryData(){
+    List<Map<String,dynamic>>summary=[];
+    for(int i=0;i<onChoose.length;i++){
+      summary.add({
+        'question_index': i,
+        'question':questions[i].question,
+        'correct_ans':questions[i].options[0],
+        'user_ans':onChoose[i],
+      }
+      );
+    }
+    return summary;
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    int index = 0;
-    print(onChoose);
+    print(getSummaryData());
     return Center(
       child: Container(
         margin: const EdgeInsets.all(40),
-        child: ListView.builder(
-          itemCount: onChoose.length,
-          itemBuilder: (context, index) {
-            return Column(
-              children: [
-                Text(questions[index].question,
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white)),
-                Card(
-                  child: ListTile(
-                    title: Text(onChoose[index]),
-                    subtitle: Text(questions[index].options[0]),
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
+        child: Column(
+          children: [
+            Text('kkk'),
+            Text('k'),
+            TextButton(onPressed: (){}, child: Text('Reset')),
+          ],
+        )
       ),
     );
   }
